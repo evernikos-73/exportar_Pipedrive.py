0369406f21cadc632ff8cf1c0d1e32d764141045
evernikos-73 <evernikos@outlook.com>
2025-07-03 16:45:52 -0300

    Update exportar_negocios.py

--- exportar_negocios.py
+++ exportar_negocios.py
@@ -57,6 +57,6 @@ exportar_a_sheets(df_notes, "https://docs.google.com/spreadsheets/d/1oR_fdVCyn1c
 # 📥 Exportar actividades
 df_activities = obtener_datos_paginados("activities")
 df_activities = limpiar_dataframe(df_activities)
-exportar_a_sheets(df_activities, "https://docs.google.com/spreadsheets/d/1oR_fdVCyn1cA8zwH4XgU5VK63cZaDC3I1i3-SWaUT20/edit", "Pipedrive activities")
+exportar_a_sheets(df_activities, "https://docs.google.com/spreadsheets/d/1oR_fdVCyn1cA8zwH4XgU5VK63cZaDC3I1i3-SWaUT20/edit", "Pipedrive Activities")
 
 print("🎉 Exportación completa")
